fc5b3231dc86f9a7045a90868f25cee398d8c709
SALMAN <elfadili.salman@gmail.com>
2019-03-15 11:00:54 +0100

    Connect and update database successfully

--- src/BoxerGym/BoxerBundle/Controller/DefaultController.php
+++ src/BoxerGym/BoxerBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace BoxerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BoxerBundle\Entity\Boxer;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BoxerBundle:Default:index.html.twig');
+        $boxer1 = new Boxer("Salman","salman@gmail.com","SalmanPass");
+        $boxer2 = new Boxer("Boxer2","boxer2@gmail.com","Boxer2Pass");
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($boxer1);
+        $em->persist($boxer2);
+        $em->flush();
+
+        return $this->render(
+            'BoxerBundle:Default:index.html.twig',
+            ['boxers' => $em->getRepository(Boxer::class)->findAll()]
+        );
+    }
+
+    public function profileAction()
+    {
+        return $this->render('BoxerBundle:Default:profile.html.twig');
     }
 }
