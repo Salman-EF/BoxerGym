5d897af96ce12fd0360b3abd6d2965de120d35f4
SALMAN <elfadili.salman@gmail.com>
2019-03-15 12:44:52 +0100

    Setup BoxerRepository with findByEmail,findAllOrderByName,createBoxer functions

---
+++ src/BoxerGym/BoxerBundle/Repository/BoxerRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BoxerBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class BoxerRepository extends EntityRepository
+{
+    
+    public function findByEmail($email) {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT b FROM BoxerBundle:Boxer b WHERE b.email = :email'
+            )
+            ->setParameter('email',$email)
+            ->getOneOrNullResult();
+    }
+
+    public function findAllOrderByName() {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT b FROM BoxerBundle:Boxer b ORDER BY b.name'
+            )
+            ->getResult();
+    }
+
+    public function createBoxer($boxer) {
+        $em = $this->getEntityManager();
+        $em->persist($boxer);
+        $em->flush();
+
+        return $boxer->getId();
+    }
+}
